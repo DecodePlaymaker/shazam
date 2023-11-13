ada5039513a36ae99c66e23561343da0e853433a
DecodePlaymaker <Chrissanchez3d@gmail.com>
2023-11-12 21:27:28 -0800

    Create function to for password generator

--- develop/script.js
+++ develop/script.js
@@ -1,51 +1,51 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var specialCharacters = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',','(',')','{','}','[',']','~','-','_','.'];
-
 var lowerCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
 var upperCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
 var numericCharacters = ['0','1','2','3','4',"5",'6','7','8','9'];
 
+var specialCharacters = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',','(',')','{','}','[',']','~','-','_','.'];
 
+// function code to help run my generator web
 function getPasswordOptions() {
 
   var length = parseInt(
-    prompt('How many characters would you like your password to contain?'),
+    prompt('Enter the number amount for password?'),
     10
   );
   
   if (Number.isNaN(length)) {
     alert('Password length must be provided as a number');
-    return null;
+    return;
   }
   
   if (length < 8) {
     alert('Password length must be at least 8 characters');
-    return null;
+    return;
   }
   
   if (length > 128) {
     alert('Password length must be less than 128 characters');
-    return null;
+    return;
   }
   
   var hasSpecialCharacters = confirm(
-    'Click OK to confirm including special characters.'
+    'Click OK to include special characters.'
   );
   
   var hasNumericCharacters = confirm(
-    'Click OK to confirm including numeric characters.'
+    'Click OK to include numeric.'
   );
   
   var hasLowerCharacters = confirm(
-    'Click OK to confirm including lowercase characters.'
+    'Click OK to include lowercase.'
   );
   
   var hasUpperCharacters = confirm(
-    'Click OK to confirm including uppercase characters.'
+    'Click OK to include uppercase.'
   );
  
   if (hasSpecialCharacters) {
@@ -56,7 +56,7 @@ function getPasswordOptions() {
     alert('Password must include at least one character type.')
   }
   
-  var passwordOptions = {
+  var passwordChoice = {
     length: length,
     hasSpecialCharacters: hasSpecialCharacters,
     hasNumericCharacters: hasNumericCharacters,
@@ -64,40 +64,39 @@ function getPasswordOptions() {
     hasUpperCharacters: hasUpperCharacters
   };
 
-  return passwordOptions;
+  return passwordChoice;
 }
 
-// Function for getting a random element from an array
 function getRandom(arr) {
-  var randIndex = Math.floor(Math.random() * arr.length);
-  var randElement = arr[randIndex];
+  var randomArr = Math.floor(Math.random() * arr.length);
+  var randomMath = arr[randomArr];
 
-  return randElement;
+  return randomMath;
 }
 
 function generatePassword() {
-  var options = getPasswordOptions();
+  var choice = getPasswordOptions();
   
-  var currentArr = new Array()
+  var push = new Array()
   
-  if (options.hasSpecialCharacters) {
-    currentArr= currentArr.concat(specialCharacters)
+  if (choice.hasSpecialCharacters) {
+    push= push.concat(specialCharacters)
   }
-  if (options.hasNumericCharacters) {
-    currentArr= currentArr.concat(numericCharacters)
+  if (choice.hasNumericCharacters) {
+    push= push.concat(numericCharacters)
   }
-  if (options.hasLowerCharacters) {
-    currentArr= currentArr.concat(lowerCharacters)
+  if (choice.hasLowerCharacters) {
+    push= push.concat(lowerCharacters)
   }
-  if(options.hasUpperCharacters) {
-    currentArr= currentArr.concat(upperCharacters)
+  if(choice.hasUpperCharacters) {
+    push= push.concat(upperCharacters)
   }
-  console.log(currentArr)
+  console.log(push)
   
   let pass = ""
   let i = 0
-  while (i < options.length) {
-    pass += getRandom(currentArr);
+  while (i < choice.length) {
+    pass += getRandom(push);
     i++
   } 
   return pass
